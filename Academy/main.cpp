3223fd96be9461990e03b1daf51d4af4dc21096c
Dima Nuss <dimanus1984@gmail.com>
2021-06-29 10:18:15 +0400

    Pointers to base class

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -5,6 +5,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define tab "\t"
+#define delimiter "\n--------------------------------------------------------------\n"
+
 class Human
 {
 	string last_name;
@@ -46,16 +49,22 @@ public:
 		set_age(age);
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human()
+	virtual ~Human()
 	{
 		cout << "HDestructor:\t" << this << endl;
 	}
 	//Methods:
-	void print()
+	virtual void print()
 	{
 		cout << last_name << " " << first_name << " " << age << " лет." << endl;
 	}
 };
+
+ostream& operator<<(ostream& os, const Human& obj)
+{
+	os << obj.get_last_name() << " " << obj.get_first_name() << ", " << obj.get_age() << " лет";
+	return os;
+}
 
 class Student :public Human
 {
@@ -117,6 +126,13 @@ public:
 	}
 };
 
+ostream& operator<<(ostream& os, const Student& obj)
+{
+	os << (Human&)obj << " ";
+	os << "Специальность: " << obj.get_specialty() << ", группа: " << obj.get_group() << ", успеваемость: " << obj.get_rating();
+	return os;
+}
+
 class Teacher :public Human
 {
 	string specialty;
@@ -167,6 +183,14 @@ public:
 	}
 };
 
+ostream& operator<<(ostream& os, const Teacher& obj)
+{
+	os << (Human&)obj << " ";
+	os << "Специвльность: " << obj.get_specialty()
+		<< ", опыт преподавания: " << obj.get_experience() << " лет";
+	return os;
+}
+
 class Graduate :public Student
 {
 	string diploma_project;
@@ -184,9 +208,9 @@ public:
 		const string& last_name, const string& first_name, unsigned int age,
 		const string& speciality, const string& group, double rating,
 		const string& diploma_project
-	) :Student(last_name, first_name, age, speciality, group, rating),
-		diploma_project(diploma_project)
+	) :Student(last_name, first_name, age, speciality, group, rating)
 	{
+		set_diploma_project(diploma_project);
 		cout << "GConstructor:\t" << this << endl;
 	}
 	~Graduate()
@@ -200,10 +224,19 @@ public:
 	}
 };
 
+ostream& operator<<(ostream& os, const Graduate& obj)
+{
+	os << (Student&)obj;
+	return os << ". Тема диплома: " << obj.get_diploma_project();
+}
+
+//#define INHERITANCE_BASICS
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
+#ifdef INHERITANCE_BASICS
 	Student stud("Тупенко", "Василий", 18, "РПО", "BV_123", 43.4);
 	stud.print();
 
@@ -212,4 +245,33 @@ void main()
 
 	Graduate Dima("Nuss", "Dmitriy", 37, "C++ software development", "DIM_05", 95, "Class Graduate");
 	Dima.print();
+#endif // INHERITANCE_BASICS
+
+	//					POINTERS TO BASE CLASS
+	Human* group[] =
+	{
+		new Student("Васильев", "Александр",23,"РПО","ПВ_011",90),
+		new Student("Васильева","Маргарита",25,"РПО","ПВ_011",90),
+		new Teacher("Ковтун","Олег",36,"Разработка приложений на C++",6),
+		new Student("Ивлев","Александр",25,"РПО","ПВ_011",95),
+		new Graduate("Рахманин","Николай",28,"РПО","ПВ_011",98, "Разработка обучающей игры"),
+		new Teacher("Романов","Андрей",30,"HardwarePC",5),
+		new Student("Нусс","Дмитрий",22,"РПО","ПВ_011",100),
+		new Student("Борн","Евгений",35,"РПО","ПВ_011",99),
+	};
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		//group[i]->print();
+		//cout << *group[i] << endl;
+		cout << typeid(*group[i]).name() << endl;
+		if (typeid(*group[i]) == typeid(Student))cout << *dynamic_cast<Student*>(group[i]) << endl;
+		if (typeid(*group[i]) == typeid(Teacher))cout << *dynamic_cast<Teacher*>(group[i]) << endl;
+		if (typeid(*group[i]) == typeid(Graduate))cout << *dynamic_cast<Graduate*>(group[i]) << endl;
+		cout << delimiter << endl;
+	}
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		delete group[i];
+	}
 }
